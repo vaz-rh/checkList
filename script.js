c62fdd3103255592a9c0148127c7e5b11b4eea08
Renato Vaz <renatovaz849@gmail.com>
2023-01-20 22:43:44 -0300

    bug resolvido

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
-button.addEventListener('click', add)
+button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
-  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
@@ -18,9 +18,9 @@ function add() {
 }
 
 function save() {
-  localStorage.setItem('NLWSetup@habtis', JSON.stringify(nlwSetup))
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
-nlwSetup.load()    
+nlwSetup.load()
